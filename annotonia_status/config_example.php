48ca26d5a16fcc51c66bc88e56f6403f5dea8f85
Greg Tunink <techgique@gmail.com>
2016-08-31 15:32:49 -0500

    Remove annotonia_status files forked to own repo

--- annotonia_status/config_example.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $boilerplate_url = "https://server.unl.edu/annotonia/tei/tei-boilerplate/cat.";
-  $catherletters_url = "http://cather.unl.edu/letters/";
-  $status_link_url = "https://server.unl.edu/annotonia_status";
-
-  $catherletters_dir = "/path/to/cocoon/annotonia/xml/letters/";
-  $flask_url = "http://server.unl.edu:port";
-?>
